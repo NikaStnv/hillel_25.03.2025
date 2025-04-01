9e27e68da629beca4af06098daed1e5c01ea7016
Admin <nkstnv@gmail.com>
2025-04-01 11:42:25 +0300

    commit_3.3

--- lesson2/home_work1.py
+++ lesson2/home_work1.py
@@ -1,13 +1,12 @@
 enter_users_number = input ("Введіть 4-oх значне число  використовуючи від 0 до 9 : ")
 transformation_int = int (enter_users_number)
-number_1 = int(transformation_int // 1000.01)
-number_2 = int((transformation_int % 1000.01) * 0.01)
-end2_number = int(transformation_int - (number_1 * 1000 + number_2 * 100))
-number_3 = int(end2_number // 10)
-number_4 = int(end2_number % 10)
-print(int(number_1))
-print(int(number_2))
-print(int(number_3))
-print(int(number_4))
+number_1 = transformation_int // 1000
+number_2 = (transformation_int // 100) % 10
+number_3 = (transformation_int // 10) % 10
+number_4 = transformation_int % 10
+print(number_1)
+print(number_2)
+print(number_3)
+print(number_4)
 
 
--- lesson2/home_work2.py
+++ lesson2/home_work2.py
@@ -1,12 +1,12 @@
 enter_users_numb = input ("Введіть 5-ти значне ціле число : ")
 transform_int = int (enter_users_numb)
-numb_1 = int(transform_int // 10000.01)
-numb_2 = int((transform_int % 10000.01) * 0.001)
-end3_numb = int(transform_int - (numb_1 * 10000 + numb_2 * 1000))
-numb_3 = int(end3_numb // 100.01)
-numb_4 = int((end3_numb % 100.01) * 0.1)
-numb_5 = int(transform_int - (numb_1 * 10000  + numb_2 * 1000 + numb_3 * 100 + numb_4 * 10))
-print(numb_5 , numb_4 , numb_3 , numb_2 , numb_1)
+numb_1 = transform_int // 10000
+numb_2 = (transform_int // 1000) % 10
+numb_3 = (transform_int // 100) % 10
+numb_4 = (transform_int // 10) % 10
+numb_5 = transform_int % 10
+print(numb_5 * 10000 + numb_4 * 1000 + numb_3 * 100 + numb_2 * 10 + numb_1)
+
 
 
 
