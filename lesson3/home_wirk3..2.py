bfbff8c2516f020c7be358e998a9d15b887ad52c
Admin <nkstnv@gmail.com>
2025-04-03 21:13:33 +0300

    commit_4.3

--- lesson3/home_wirk3..2.py
+++ lesson3/home_wirk3..2.py
@@ -1,3 +1,7 @@
-lst_1 = [1, -1.5, 2, -1, "python", -7, 0.6]
-lst_2 = lst_1[::-1]
-print(lst_2)
+lst = []
+element_end = lst[-1] if len(lst) != 0 else ""
+if len(lst) != 0:
+    lst.pop(-1) and lst.insert(0, element_end)
+    print(lst)
+else:
+    print(lst)
