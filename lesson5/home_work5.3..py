61a4c4819a450af64b38774611674cf598f6045b
Admin <nkstnv@gmail.com>
2025-04-14 23:13:03 +0300

    commit_5.2

--- lesson5/home_work5.3..py
+++ lesson5/home_work5.3..py
@@ -1,11 +1,12 @@
 user_str = input("Enter text: ")
-form_str1 = user_str.title()
-form_str2 = "#"
-for y in form_str1:
-    if y.isalnum():
-        form_str2 += y
-    else:
-        form_str2 += ""
-    if len(form_str2) > 139:
-        break
-print(form_str2)
+lst_str = user_str.split(" ")
+form_str = "#"
+for b in lst_str:
+    b = b[0].upper() + b[1:].lower()
+    for c  in b:
+        if c.isalnum():
+            form_str += c
+        if len(form_str) > 139:
+            break
+print(form_str)
+
