081bb5861d21000ff25bd8a28e1b983ece04eebc
Admin <nkstnv@gmail.com>
2025-04-19 07:26:41 +0300

    commit_6.3

---
+++ lesson6/home_work6.3.py
@@ -0,0 +1,21 @@
+user_data = input("Enter numbers: ")
+numbers = []
+for x in user_data:
+    numbers.append(int(x))
+while 1 in  numbers[1:]:
+    numbers.remove(1)
+dct = { y : x for y, x  in enumerate(numbers,1)}
+multiplication = 1
+a = 1
+while len(dct) >= 1:
+    if numbers.count(0) > 0:
+        multiplication *= 0
+        break
+    if a in dct:
+        multiplication *= dct.pop(a)
+        a += 1
+    if len(dct) == 0 and multiplication > 9:
+        (dct) = {y: int(x) for y, x in enumerate(str(multiplication), 1)}
+        multiplication = 1
+        a = 1
+print(multiplication)
