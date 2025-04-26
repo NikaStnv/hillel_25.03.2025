182283ce74ffe54614c726274b38fb5390f3b5af
Admin <nkstnv@gmail.com>
2025-04-27 01:41:07 +0300

    commit_9.4

--- lesson9/home_work9.1.py
+++ lesson9/home_work9.1.py
@@ -1,7 +1,6 @@
 def popular_words (text: str, words: list) -> dict:
     text = (text.lower()).split()
-    words = list(word.lower() for word in words)
-    return {key: text.count(key) for key in words}
+    return {key: text.count(key.lower()) for key in words}
 
 
 assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
