86ef01ad11a9183bfbf2a73041a13f8b741b8746
Admin <nkstnv@gmail.com>
2025-05-09 04:49:00 +0300

    commit_12.1

--- lesson12/home_work12.1.py
+++ lesson12/home_work12.1.py
@@ -1,4 +1,24 @@
-def print_array(arr):
-    return ",".join(str(x) for x in arr)
+import re
 
-print(print_array([2, 4, 5, 2]))
+def edit_file(file_path):
+    """ Очищення тексту від html-тегів та порожні рядки"""
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        content = file.read()
+        edit1_content = re.sub(r'<[^>]+>', '', content)
+        edit2_content = [line for line in edit1_content.splitlines()  if line.strip() != ""]
+
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write("\n".join(edit2_content))
+    return True
+
+
+path = r"C:\Users\Admin\PycharmProjects\Hillel_Basic\hillel_25.03.2025\lesson12\draft.html"
+edit_file(path)
+
+
+
+import codecs
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+      with codecs.open(html_file, 'r', 'utf-8') as file:
+           html = file.read()
