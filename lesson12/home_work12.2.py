cd6c6f775c48e7573f281089595ecb6dae8a14f0
Admin <nkstnv@gmail.com>
2025-05-10 01:41:44 +0300

    commit_12.5

--- lesson12/home_work12.2.py
+++ lesson12/home_work12.2.py
@@ -54,11 +54,11 @@ print(apple)
 print(banana)
 #
 buyer_1 = User("Ivan", "Ivanov", "02628162", 0)
-# buyer_2 = User('Anna', 'Melnik', '02645631', 10)
-# buyer_3 = User('Kira', 'Shevchenko', '02668422', 20)
-# print(buyer_1)  # Ivan Ivanov
-# print(buyer_2)
-# print(buyer_3)
+buyer_2 = User('Anna', 'Melnik', '02645631', 10)
+buyer_3 = User('Kira', 'Shevchenko', '02668422', 20)
+print(buyer_1)  # Ivan Ivanov
+print(buyer_2)
+print(buyer_3)
 #
 cart = Purchase(buyer_1)
 cart.add_item(lemon, 4)
@@ -67,22 +67,22 @@ print(cart)
 print(cart.get_total())
 #
 # cart.add_item(apple, 10)
-# print(cart)
-# print(cart.get_total())
-#
-# cart_2 = Purchase(buyer_2)
-# cart_2.add_item(lemon, 7)
-# cart_2.add_item(banana, 15)
-# cart_2.add_item(apple, 8)
-# print(cart_2)
-# print(cart_2.get_total())
-#
-# cart_3 = Purchase(buyer_3)
-# cart_3.add_item(lemon, 1)
-# cart_3.add_item(banana, 20)
-# cart_3.add_item(apple, 30)
-# print(cart_3)
-# print(cart_3.get_total())
+print(cart)
+print(cart.get_total())
+
+cart_2 = Purchase(buyer_2)
+cart_2.add_item(lemon, 7)
+cart_2.add_item(banana, 15)
+cart_2.add_item(apple, 8)
+print(cart_2)
+print(cart_2.get_total())
+
+cart_3 = Purchase(buyer_3)
+cart_3.add_item(lemon, 1)
+cart_3.add_item(banana, 20)
+cart_3.add_item(apple, 30)
+print(cart_3)
+print(cart_3.get_total())
 
 """
 User: Ivan Ivanov
