100269a9fb09910218f4a1d1d2a0f5a894b787a0
Admin <nkstnv@gmail.com>
2025-05-12 01:00:44 +0300

    commit_13.1

---
+++ lesson13/home_work13.1.py
@@ -0,0 +1,65 @@
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.gender}, {self.age})"
+
+
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.gender}, {self.age} age, {self.record_book})"
+
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        self.group = set(student for student in self.group if student.last_name != last_name)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = "\n".join(str(student) for student in self.group)
+        return f'Number:{self.number}\n{all_students}'
+
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st3 = Student("Female", 27, "Anna", "Smith","AN149")
+print(st1)
+print(st2)
+print(st3)
+
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+gr.add_student(st3)
+print(gr.find_student('Anna'))
+print(gr.find_student('Taylor'))
+print(gr)
+
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+
+gr.delete_student('Taylor')
+print(gr)  # Only one student
+
+gr.delete_student('Taylor')  # No error!
