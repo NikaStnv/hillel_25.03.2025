428d58ce7b291d875467a46f3c1b28eda42ca701
Admin <nkstnv@gmail.com>
2025-05-12 16:48:43 +0300

    commit_13.1

--- lesson12/home_work12.1.py
+++ lesson12/home_work12.1.py
@@ -20,6 +20,8 @@ edit_file(path)
 
 
 import codecs
+
+
 def delete_html_tags(html_file, result_file='cleaned.txt'):
       with codecs.open(html_file, 'r', 'utf-8') as file:
            html = file.read()
